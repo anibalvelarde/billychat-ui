93d36d9babfe5bd53a383309a6b2e6ba9eb80444
AEV0129 <AEV0129@daugherty.com>
2020-05-16 11:25:18 -0500

    added the billychat name.

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -96,7 +96,7 @@ export default function Admin({ ...rest }) {
     <div className={classes.wrapper}>
       <Sidebar
         routes={routes}
-        logoText={"Creative Tim"}
+        logoText={"BILL Y CHAT"}
         logo={logo}
         image={image}
         handleDrawerToggle={handleDrawerToggle}
