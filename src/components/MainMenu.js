b7dd8a3097e1aed082f85fb0b98abb51dfa7472d
AEV0129 <AEV0129@daugherty.com>
2020-05-31 15:58:04 -0500

    Added first version of menu that shows only when logged in.

---
+++ src/components/MainMenu.js
@@ -0,0 +1,116 @@
+import React from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import SendIcon from "@material-ui/icons/Send";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const StyledMenu = withStyles({
+  paper: {
+    border: "1px solid #d3d4d5"
+  }
+})(props => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "center"
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "center"
+    }}
+    {...props}
+  />
+));
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+const StyledMenuItem = withStyles(theme => ({
+  root: {
+    "&:focus": {
+      backgroundColor: theme.palette.primary.main,
+      "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
+        color: theme.palette.common.white
+      }
+    }
+  }
+}))(MenuItem);
+
+export default function CustomizedMenus() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const classes = useStyles();
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      {/* <Button
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        color="primary"
+        onClick={handleClick}
+      >
+        Open Menu
+      </Button> */}
+      <IconButton
+        edge="start"
+        className={classes.menuButton}
+        color="inherit"
+        aria-label="menu"
+        onClick={handleClick}
+      >
+        <MenuIcon />
+      </IconButton>
+      <StyledMenu
+        id="customized-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledMenuItem>
+          <ListItemIcon>
+            <SendIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Sent mail" />
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <DraftsIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Drafts" />
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <InboxIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Inbox" />
+        </StyledMenuItem>
+      </StyledMenu>
+    </div>
+  );
+}
---
+++ src/components/NewNavBar.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { useAuth0 } from "../authn-authr/react-auth0-spa";
+import CustomizedMenus from "./MainMenu";
+import PropTypes from "prop-types";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+const CallToAction = ({ isAuthenticated }) => {
+  const { loginWithRedirect, logout } = useAuth0();
+  console.log({
+    from: "CallToAction",
+    isAuthenticated
+  });
+
+  if (isAuthenticated) {
+    return <button onClick={() => logout()}>Log out</button>;
+  }
+  return <button onClick={() => loginWithRedirect({})}>Log in</button>;
+};
+
+const MainMenu = ({ isAuthenticated }) => {
+  const classes = useStyles();
+  if (!isAuthenticated) return null;
+
+  return <CustomizedMenus />;
+  //   return (
+  //     <IconButton
+  //       edge="start"
+  //       className={classes.menuButton}
+  //       color="inherit"
+  //       aria-label="menu"
+  //       disabled={!isAuthenticated}
+  //     >
+  //       <MenuIcon />
+  //     </IconButton>
+  //   );
+};
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+  const { isAuthenticated } = useAuth0();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <MainMenu isAuthenticated={isAuthenticated} />
+          <Typography variant="h6" className={classes.title}>
+            www.billychat.com
+          </Typography>
+          <CallToAction isAuthenticated={isAuthenticated} />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+MainMenu.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+CallToAction.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
