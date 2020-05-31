24c7059d05f304d5cc6f843dae6e38aa2dc8c02d
Anibal Velarde <anibalvelarde@gmail.com>
2020-05-31 16:27:40 -0500

    New beginnings (#20)
    * New beginnings...
    
    * Added first version of menu that shows only when logged in.
    
    * Fixed some eslint errors.
    
    Co-authored-by: AEV0129 <AEV0129@daugherty.com>

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React from "react";
-import NavBar from "./components/NavBar";
+// import NavBar from "./components/NavBar";
 import NewNavBar from "./components/NewNavBar";
 
 // New - import the React Router components, and the Profile page component
